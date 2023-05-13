5adc7c2b18a81130fe884d2bb92521cf04732703
guo <1822098581@qq.com>
2023-05-13 10:40:45 +0800

    add train,val,test accuracy

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ from utils.acc import accuracy
 train_data_std, train_label, val_data_std, val_label, test_data_std, test_label = read()
 model = LogisticRegression(train_data_std, train_label)  # logistic regression fit
 
-train_pred = LogisticRegression_pred(model,train_data_std)
-train_acc = accuracy(train_pred,train_label)
-print("train dataset accuracy:",train_acc)
+train_pred = LogisticRegression_pred(model, train_data_std)
+train_acc = accuracy(train_pred, train_label)
+print("train dataset accuracy:", train_acc)
 
-val_pred = LogisticRegression_pred(model,val_data_std)
-val_acc = accuracy(val_pred,val_label)
-print("val dataset accuracy:",val_acc)
+val_pred = LogisticRegression_pred(model, val_data_std)
+val_acc = accuracy(val_pred, val_label)
+print("val dataset accuracy:", val_acc)
 
 test_pred = LogisticRegression_pred(model, test_data_std)
 test_acc = accuracy(test_pred, test_label)
-print("test dataset accuracy:",test_acc)
-
-
+print("test dataset accuracy:", test_acc)
