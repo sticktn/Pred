71c8d2adaa13a3cc81433cd03f6abbaaa644799d
sticktn <1822098581@qq.com>
2023-05-23 14:36:12 +0800

    add precision,recall,f1

--- utils/acc.py
+++ utils/acc.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def accuracy(pred, test_label):
     """
     Computational model accuracy
@@ -8,3 +11,23 @@ def accuracy(pred, test_label):
     length = len(test_label)
     accuracy = (pred == test_label).sum() / length
     return accuracy
+
+
+def evaluation(pred, test_label):
+    """
+    Evaluation of classification model
+    :param pred: predictive classification
+    :param test_label: true classification
+    :return: Evaluation of classification model
+    """
+    TP = ((test_label == 1) & (pred == 1)).sum()
+    TN = ((test_label == 0) & (pred == 0)).sum()
+    FP = ((test_label == 0) & (pred == 1)).sum()
+    FN = ((test_label == 1) & (pred == 0)).sum()
+    df = pd.DataFrame([[TP, FP], [TN, FN]], columns=['T', 'F'], index=['P', 'N'])
+    print(df)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    F1 = 2 * precision * recall / (precision + recall)
+    return accuracy, precision, recall, F1
