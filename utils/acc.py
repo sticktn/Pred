26d1d04fdb6b241d8d9bb9a05900f2d1b255b862
guo <1822098581@qq.com>
2023-05-27 18:55:14 +0800

    add train,val,test accuracy

--- utils/acc.py
+++ utils/acc.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.metrics import confusion_matrix
 
 
 def accuracy(pred, test_label):
@@ -26,8 +27,14 @@ def evaluation(pred, test_label):
     FN = ((test_label == 1) & (pred == 0)).sum()
     df = pd.DataFrame([[TP, FP], [TN, FN]], columns=['T', 'F'], index=['P', 'N'])
     print(df)
+    # print(confusion_matrix(test_label, pred)) # 直接调用sklearn的混淆矩阵api
     precision = TP / (TP + FP)
     recall = TP / (TP + FN)
     accuracy = (TP + TN) / (TP + TN + FP + FN)
     F1 = 2 * precision * recall / (precision + recall)
     return accuracy, precision, recall, F1
+
+
+def print_evaluation(pred, label):
+    Accuracy, Precision, Recall, F1 = evaluation(pred, label)
+    print("Accuracy:", Accuracy, "Precision:", Precision, "Recall:", Recall, "F1:", F1)
