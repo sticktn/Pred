9799fcd302d92729fb5d61634e4ea0da2fcc335d
guo <1822098581@qq.com>
2023-05-27 19:25:44 +0800

    DecisionTree model example

--- DecisionTree_main.py
+++ DecisionTree_main.py
@@ -6,21 +6,25 @@
 @Date    ：2023/5/17 下午6:08 \n
 """
 from utils.read_data import read
-from utils.DecisionTree import DecisionTreeClassify,DecisionTree_Pred
-from utils.acc import accuracy
-
+from utils.DecisionTree import DecisionTreeClassify, DecisionTree_Pred
+from utils.acc import print_evaluation
+from sklearn.tree import export_text
+import joblib
 
 train_data_std, train_label, val_data_std, val_label, test_data_std, test_label = read()
 model = DecisionTreeClassify(train_data_std, train_label, criterion="gini")
 
 train_pred = DecisionTree_Pred(model, train_data_std)
-train_acc = accuracy(train_pred, train_label)
-print("train dataset accuracy:", train_acc)
+print("train dataset evaluation:")
+print_evaluation(train_pred, train_label)
 
 val_pred = DecisionTree_Pred(model, val_data_std)
-val_acc = accuracy(val_pred, val_label)
-print("val dataset accuracy:", val_acc)
+print("val dataset evaluation:")
+print_evaluation(val_pred, val_label)
 
 test_pred = DecisionTree_Pred(model, test_data_std)
-test_acc = accuracy(test_pred, test_label)
-print("test dataset accuracy:", test_acc)
+print("test dataset evaluation:")
+print_evaluation(test_pred, test_label)
+
+print(export_text(model))
+joblib.dump(model, "DecisionTree.pkl")
